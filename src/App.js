d4f3ed3b4594ec5153412ea314ee9e2af2fb8bb2
Parham Abolghasemi <parhamab17@gmail.com>
2022-04-17 11:18:10 +0430

    styles, search componetnt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import AddNotes from "./components/AddNotes";
+import Note from "./components/Note";
+import NotesList from "./components/NotesList";
+import Search from "./components/Search";
 
 const App = () => {
+  const [searchedTxt, setSearchedTxt] = useState("");
+  const [notes, setNotes] = useState([
+    { id: uuidv4(), text: "aa", date: "15/04/2022" },
+    { id: uuidv4(), text: "bb", date: "15/04/2022" },
+    { id: uuidv4(), text: "cc", date: "15/04/2022" },
+  ]);
+
   return (
     <div>
-      
+      <Search handleSearchText={setSearchedTxt} />
+      <Note />
+      <NotesList />
+      <AddNotes />
     </div>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Form } from "react-bootstrap";
 
-const Search = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Search = ({ setSearchedTxt }) => {
+  return (
+    <div className="container my-5">
+      <Form.Group className="mb-3">
+        <Form.Control
+          type="search"
+          placeholder="Search...."
+        //   onChange={(e) => handleSearchText(e.target.value)}
+        />
+      </Form.Group>
+    </div>
+  );
 };
 
 export default Search;
