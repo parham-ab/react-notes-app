c52b0117d3916d379b097a696d133942596f7a2a
Parham Abolghasemi <parhamab17@gmail.com>
2022-04-17 12:08:25 +0430

    search handler, note limitation

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 
-const Search = ({ setSearchedTxt }) => {
+const Search = ({ handleSearchText }) => {
   return (
     <div className="container my-5">
       <Form.Group className="mb-3">
         <Form.Control
           type="search"
           placeholder="Search...."
-        //   onChange={(e) => handleSearchText(e.target.value)}
+          onChange={(e) => handleSearchText(e.target.value)}
         />
       </Form.Group>
     </div>
