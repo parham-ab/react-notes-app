280723e8baee6430ea6715d419c321ebd06518a1
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-03 09:49:08 +0430

    saving data to localstorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { MdNorthEast } from "react-icons/md";
 import { v4 as uuidv4 } from "uuid";
 // components
 import NotesList from "./components/NotesList";
@@ -7,7 +8,12 @@ import Search from "./components/Search";
 const App = () => {
   const [searchedTxt, setSearchedTxt] = useState("");
   const [notes, setNotes] = useState([]);
-
+  // get saved data from localStorage & display it
+  useEffect(() => {
+    const savedData = localStorage.getItem("react-notes-list");
+    const parsedData = JSON.parse(savedData);
+    setNotes(parsedData);
+  }, []);
   // add new notes
   const addNote = (text) => {
     const date = new Date();
@@ -18,11 +24,20 @@ const App = () => {
     };
     const newNotes = [...notes, newNote];
     setNotes(newNotes);
+    // save to localStorage
+    localStorage.setItem(
+      "react-notes-list",
+      JSON.stringify([...notes, newNote])
+    );
+    console.log(newNote);
   };
+  // load from localStorage
   // delete notes
   const deleteNote = (id) => {
     const newNotes = notes.filter((item) => item.id !== id);
     setNotes(newNotes);
+    // remove from localStorage
+    localStorage.setItem("react-notes-list", JSON.stringify(newNotes));
   };
 
   return (
