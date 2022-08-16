72ecb90eee27c6ce96b0014b83bd1f8a711a3ede
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-16 09:59:58 +0430

    remaining components refactored with MUI components

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,23 +1,39 @@
 import React from "react";
-import { Badge } from "react-bootstrap";
 // icons
 import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, Chip, Divider, IconButton, Typography } from "@mui/material";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
 const Note = ({ id, text, date, handleDeleteNote }) => {
   return (
-    <div className="p-3 rounded m-3 shadow note">
-      <span>{text}</span>
-      <div>
-        <hr />
-        <Badge pill bg="dark">
-          {date}
-        </Badge>
-        <DeleteIcon
-          className="text-danger m-2 deleteIcon"
+    <Box
+      className="p-3 rounded m-3 note"
+      sx={{ boxShadow: "0 0.5rem 1rem rgb(0 0 0 / 15%)" }}
+    >
+      <Box component="div">
+        <Typography variant="" color="initial">
+          {text}
+        </Typography>
+      </Box>
+      <Divider />
+      <Box
+        component="div"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        mt={2}
+      >
+        <Chip icon={<AccessTimeIcon />} label={date} />
+
+        <IconButton
           onClick={() => handleDeleteNote(id)}
-        />
-      </div>
-    </div>
+          color="error"
+          size="small"
+        >
+          <DeleteIcon />
+        </IconButton>
+      </Box>
+    </Box>
   );
 };
 
