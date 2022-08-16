95e76728c3ab4306a49d5290a2209a260e5d517e
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-16 10:09:12 +0430

    folder structure updated, bugs fixed

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,13 +1,19 @@
+// MUI
+import { Box, Chip, Divider, IconButton, Typography } from "@mui/material";
 // icons
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Box, Chip, Divider, IconButton, Typography } from "@mui/material";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
 const Note = ({ id, text, date, handleDeleteNote }) => {
   return (
     <Box
       className="p-3 rounded m-3 note"
-      sx={{ boxShadow: "0 0.5rem 1rem rgb(0 0 0 / 15%)" }}
+      sx={{
+        boxShadow: "0 0.5rem 1rem rgb(0 0 0 / 15%)",
+        borderRadius: "0.25rem",
+      }}
+      p={3}
+      m={3}
     >
       <Box component="div">
         <Typography variant="" color="initial">
@@ -23,7 +29,6 @@ const Note = ({ id, text, date, handleDeleteNote }) => {
         mt={2}
       >
         <Chip icon={<AccessTimeIcon />} label={date} />
-
         <IconButton
           onClick={() => handleDeleteNote(id)}
           color="error"
